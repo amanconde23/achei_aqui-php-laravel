6d9fe8d794c98f178a5575b540111f17dea93405
amanconde23 <amanconde23@gmail.com>
2020-08-27 16:56:23 -0300

    teste banco de dados com relacionamento one to many laravel

---
+++ app/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'name', 'category',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ resources/views/listarProdutos.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Produtos Cadastrados</title>
+</head>
+<body>
+    <h1>Listar Produtos Cadastrados</h1>
+
+    <table>
+        <tr>
+            <td>ID</td>
+            <td>Nome:</td>
+            <td>Categoria:</td>
+            <td>Proprietário:</td>
+            <td>Telefone:</td>
+        </tr>
+
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->category }}</td>
+            <td>{{ $product->user->name }}</td>
+            <td>{{ $product->user->phone }}</td>
+        </tr>
+        @endforeach
+    </table>
+
+</body>
+</html>
