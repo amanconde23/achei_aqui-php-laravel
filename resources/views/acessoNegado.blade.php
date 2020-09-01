24bfbc197e941100f9c112a67a3f1ae67ba28129
amanconde23 <amanconde23@gmail.com>
2020-09-01 08:38:19 -0300

    organizando rotas e criado pagina de acesso negado

---
+++ resources/views/acessoNegado.blade.php
@@ -0,0 +1,2 @@
+<h1>Acesso Negado! Você não tem permissão para acessar esse conteúdo!</h1>
+<a href="{{ route('products') }}">Voltar para a sua página</a>
--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// **HOME ** //
+Route::get('/home', 'HomeController@index')->name('home');
+// **END HOME ** //
+
+
+// **ROTAS DE LOGIN ** //
+Auth::routes();
+// **END ROTAS DE LOGIN ** //
+
+
+// **SHOW ADMIN PRODUCTS ** //
 Route::group(['middleware' => ['auth','admin']], function(){
     Route::get('admin-produtos', 'ProductController@showProductsAdmin');
 });
+// **END ADMIN PRODUCTS ** //
 
-// listar todos os produtos do usuario
-Route::get('listar-produtos', 'ProductController@index')->name('products');
 
-// listar todos os produtos
-Route::get('listar-todos-produtos', 'ProductController@showAllProducts')->name('all-products');
+// **ACCESS DENIED PAGE ** //
+Route::get('access-denied', function () {
+    return view('acessoNegado');
+});
+// **END ACCESS DENIED PAGE ** //
 
-// formulario de editar produto
-Route::get('produto/editarform/{product}', 'ProductController@editProductForm')->name('product-edit-form');
 
-// ação de editar produto
-Route::put('produto/edit/{product}', 'ProductController@update')->name('product-edit');
-
-// mostrar detalhes de um produto
-Route::get('product/{product}', 'ProductController@showOneProduct')->name('product-show-details');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
+// **SEARCHBAR PRODUCT ** //
 // ação de buscar
 Route::get('/search', 'ProductController@search')->name('search-results');
 
@@ -47,12 +50,40 @@ Route::get('/search', 'ProductController@search')->name('search-results');
 Route::get('/resultado-busca', function () {
     return view('resultadoBusca');
 });
+// **END SEARCHBAR PRODUCT ** //
 
+
+// **SHOW PRODUCT** //
+// listar todos os produtos
+Route::get('listar-todos-produtos', 'ProductController@showAllProducts')->name('all-products');
+
+// listar todos os produtos do usuario
+Route::get('listar-produtos', 'ProductController@index')->name('products');
+
+// mostrar detalhes de um produto
+Route::get('product/{product}', 'ProductController@showOneProduct')->name('product-show-details');
+// **END SHOW PRODUCT ** //
+
+
+// **CREATE PRODUCT** //
 // formulario de criar novo produto
 Route::get('produto/novo', 'ProductController@create')->name('product-new');
 
 // ação de criar e armazenar novo produto
 Route::post('produto/store', 'ProductController@store')->name('product-store');
+// **END CREATE PRODUCT ** //
 
+
+// **UPDATE PRODUCT** //
+// formulario de editar produto
+Route::get('produto/editarform/{product}', 'ProductController@editProductForm')->name('product-edit-form');
+
+// ação de editar produto
+Route::put('produto/edit/{product}', 'ProductController@update')->name('product-edit');
+// **END UPDATE PRODUCT ** //
+
+
+// **DELETE PRODUCT** //
 // ação de apagar produto
 Route::delete('produto/destroy/{product}', 'ProductController@destroy')->name('product-destroy');
+// **END DELETE PRODUCT ** //
