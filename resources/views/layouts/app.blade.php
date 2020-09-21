5f183400b33c1ea0c8308b152bbbe2aa2b00482a
amanconde23 <amanconde23@gmail.com>
2020-09-21 13:11:51 -0300

    adicionado bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('site/jquery.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,7 +21,7 @@
     <script src="https://kit.fontawesome.com/a0b66fbad1.js" crossorigin="anonymous"></script>
 
     <!-- Styles -->
-    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('site/bootstrap.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
