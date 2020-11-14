498da424f2c7c5117f841b87837bcab94c71e4fb
amanconde23 <amanconde23@gmail.com>
2020-11-14 18:52:46 -0300

    alteração no texto da home

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,7 +57,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password"><p>Mínimo 8 caracteres</p>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,7 +67,8 @@
             <div class="col-md-12 col-sm-8">
                 <h3 class="home-about-us-h3">Sobre Nós</h3>
                 <p class="home-about-us-p">
-                    Buscamos oferecer um ambiente seguro de compartilhamento de coisas. Sabe aquele objeto que você comprou, usou uma vez, e deixou encostado em um canto? Que tal repassá-lo para alguém que irá fazer bom uso dele? Vamos promover a economia colaborativa e deixar de lado o consumismo!
+                    Buscamos oferecer um ambiente seguro de compartilhamento de objetos. Sabe aquele objeto que você comprou, usou uma vez, e deixou encostado em um canto? Que tal repassá-lo para alguém que irá fazer bom uso dele? Vamos promover a economia colaborativa e deixar de lado o consumismo!
+                    Aqui você pode encontrar um objeto que esteja precisando, entrar em contato com o proprietário, e combinar a doação!
                 </p>
                 <a class="btn-participar-home" href="{{ route('register') }}">Participar</a>
             </div>
